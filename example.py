0aaacc08084e8c5c7a187361ecbf35d416b2123b
Jon Craton <jncraton@gmail.com>
2024-10-23 09:27:05 -0400

    Add example.py

---
+++ example.py
@@ -0,0 +1,12 @@
+# An example program with 3 source lines of code
+# This program has 5 comments and 5 blank lines
+
+print("Hello, world")
+
+# This is a comment
+
+if False:
+    # Indented comments should be handled properly
+    print("This won't print")
+    
+# Another comment
